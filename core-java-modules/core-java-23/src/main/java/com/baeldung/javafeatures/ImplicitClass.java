b2622fa213395f9ddedd92b142e6a9f3830320d7
Chathuranga Dassanayake <chathura2ihl@gmail.com>
2024-10-22 23:45:42 +0530

    delete unused

--- core-java-modules/core-java-23/src/main/java/com/baeldung/javafeatures/ImplicitClass.java
+++
@@ -1,4 +0,0 @@
-void main() {
-    String name = readln("Please enter your name: ");
-    print("Hello "+name);
-}
--- spring-cloud-modules/spring-cloud-bootstrap/gateway/src/main/resources/static/home/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-FG4YK6QA.mjs";import"./chunk-TVQO2A6G.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
